da7abd9fde654a6260b1f78628396e3c68e9a057
Rickson Lima <lima.rickson20@gmail.com>
2021-09-12 01:39:53 -0400

    feat: adding cors support

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import routes from './routes'
 
@@ -10,13 +11,14 @@ const server = express()
 const port = process.env.PORT || 3000
 
 mongoose.connect(
-    `mongodb+srv://dbAdmin:${process.env.DB_PASSWORD}@clusterbloggy.g1t2e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
+  `mongodb+srv://dbAdmin:${process.env.DB_PASSWORD}@clusterbloggy.g1t2e.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
 )
 
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 
 server.listen(port, () => {
-    console.log(`API is running on http://localhost:${port}`)
-  })
+  console.log(`API is running on http://localhost:${port}`)
+})
